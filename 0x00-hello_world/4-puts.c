e49d5cbd0bbc277798027843bce6ee6b5af4f94a
Slamawit-Kassaye <selamkassaye2019.com>
2022-09-08 12:21:06 -0700

    low_levlel_programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include<studio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+#include<stdio.h>
+    /**
+    * main - Entry point
+    *
+    * Return: Always 0 (Success)
+    */
 int main(void)
 {
-	puts("\"programming is like bulinding a multilingual puzzle");
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }
