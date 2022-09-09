c275d7c7b6e844ce0c6bbd75d5551d73fe746d0e
Slamawit-Kassaye <selamkassaye2019.com>
2022-09-09 15:20:12 -0700

    variables, if, else

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,13 +14,10 @@ int main(void)
 		putchar(i);
 		if (i != 57)
 		{
-			putchar(i);
-			if (i != 57)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			putchar(',');
+			putchar(' ');
 		}
-		putchar('\n');
-		return (0);
 	}
+	putchar('\n');
+	return (0);
+}
