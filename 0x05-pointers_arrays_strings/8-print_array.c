5a6a26096cb7f39eee6be3d7256596474578fb59
Slamawit-Kassaye <selamkassaye2019.com>
2022-09-19 10:41:40 -0700

    pointers, arrays, strings

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,7 +4,8 @@
 /**
  * print_array - prints n, elements of an array of integers
  * followed by a new line.
- * @a: input n elements.
+ * @a: input array.
+ * @n: input n elements.
  * Return: no return.
  */
 void print_array(int *a, int n)
